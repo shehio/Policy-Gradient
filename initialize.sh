e86ad94575c1d2b94fa654d4f6f6db1d8f04a6b8
Shehab Yasser <shehabyasser@gmail.com>
2024-07-10 15:54:06 -0400

    Fixed version in initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,11 +1,19 @@
-REPOSRC="hclone https://github.com/official-stockfish/Stockfish.git"
-LOCALREPO="Stockfish"
+#!/bin/bash
+repo_src="hclone https://github.com/official-stockfish/Stockfish.git"
+local_repo="Stockfish"
 
-if [ ! -d "$LOCALREPO/.git" ]; then
-  git clone "$REPOSRC" "$LOCALREPO"
+if [ ! -d "$local_repo/.git" ]; then
+  git clone "$repo_src" "$local_repo"
 else
-  git -C "$LOCALREPO" pull
+  git -C "$local_repo" pull
 fi
 
 cd Stockfish/src
-make build ARCH=x86-64
+
+file="misc.cpp"
+old_string='constexpr std::string_view version = "dev";'
+new_string='constexpr std::string_view version = "16";'
+sed -i '' "s|$old_string|$new_string|g" "$file"
+
+make clean
+make build ARCH=apple-silicon
