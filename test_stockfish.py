cf21a72be1c6c3f9b00bc8e720612f8ef6f8821e
Shehab Yasser <shehabyasser@gmail.com>
2024-07-10 16:29:37 -0400

    Added test_stockfish.py

---
+++ test_stockfish.py
@@ -0,0 +1,29 @@
+from stockfish import Stockfish
+
+
+positions = [
+    {"fen": "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1", "expected_evaluation": {'type': 'cp', 'value': 107}},
+    {"fen": "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "expected_evaluation": {'type': 'cp', 'value': 31}},
+    {"fen": "r1bqkbnr/pppppppp/n7/8/8/N7/PPPPPPPP/R1BQKBNR w KQkq - 0 1", "expected_evaluation": {'type': 'cp', 'value': 16}}
+]
+
+depth = 30
+stockfish = Stockfish(path="./Stockfish/src/stockfish")
+stockfish.set_depth(depth)
+print(f"The current major version of stockfish is: {stockfish.get_stockfish_major_version()}\n")
+
+for position in positions:
+    fen = position["fen"]
+
+    stockfish.set_fen_position(fen)
+    actual_evaluation = stockfish.get_evaluation()
+    best_move = stockfish.get_best_move()
+    
+    print(stockfish.get_board_visual())
+    print(f"The current best move is {best_move}.\n")
+
+    expected_evaluation = position["expected_evaluation"]
+    if actual_evaluation == expected_evaluation:
+        print("The evaluation matches the known benchmark.\n")
+    else:
+        print("The evaluation does not match the known benchmark.\n")
