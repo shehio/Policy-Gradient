f0e07b6e41fc408f7ec6cf3c7a79e34710f9d57c
Shehab Yasser <shehabyasser@gmail.com>
2024-07-11 10:08:18 -0400

    Separated homebrew

--- build_leela.sh
+++ build_leela.sh
@@ -8,23 +8,7 @@ else
   git -C "$local_repo" pull
 fi
 
-pretty_print() {
-  printf "\n%b\n" "$1"
-}
-
-if ! command -v brew &> /dev/null; then
-  pretty_print "Homebrew is not installed. Installing Homebrew..."
-  
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  
-  # Add Homebrew to the PATH
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-  
-  pretty_print "Homebrew installation completed."
-else
-  pretty_print "Homebrew is already installed."
-fi
+bash install_homebrew.sh
 
 brew install meson ninja python3 zlib gcc
 cd lc0 && mkdir build && cd build
