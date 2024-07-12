29b4e578f89f58023f815502683c25193baa5c60
Shehab Yasser <shehabyasser@gmail.com>
2024-07-12 08:21:24 -0400

    Updated build_leela.sh

--- engines/build_leela.sh
+++ engines/build_leela.sh
@@ -6,3 +6,5 @@ brew install meson ninja python3 zlib gcc
 cd lc0 && mkdir build && cd build
 meson setup --buildtype=release ..
 ninja
+
+wget  --tries=10 -P lc0/build/ https://storage.lczero.org/files/networks-contrib/t1-512x15x8h-distilled-swa-3395000.pb.gz
