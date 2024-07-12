dc496e6c32c017a1837eda8dbcfa6fbf4f57808e
Shehab Yasser <shehabyasser@gmail.com>
2024-07-12 16:53:20 -0400

    Added test_lc0.py

--- lc0_client.py
+++ lc0_client.py
@@ -44,18 +44,3 @@ class Lc0Client:
                 break
             response.append(line)
         return response
-
-if __name__ == "__main__":
-    lc0_path = './lc0/build/lc0'
-    weights_path = './lc0/build/t1-512x15x8h-distilled-swa-3395000.pb.gz'
-
-    client = Lc0Client(lc0_path, weights_path)
-    client.start_engine()
-
-    moves = ['e2e4', 'e7e5', 'g1f3', 'b8c6']
-    client.set_position(moves)
-
-    response = client.go(nodes=100)
-    print(f"Engine response: {response}\n")
-
-    client.stop_engine()
