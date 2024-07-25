71fa15dc96e8ec42c8f4a60a843492f9327d00b1
Shehab Yasser <shehabyasser@gmail.com>
2024-07-25 17:29:22 -0400

    Updated river-crossing-puzzle.py

--- river-crossing-puzzle.py
+++ river-crossing-puzzle.py
@@ -8,3 +8,13 @@ class State:
 
     def is_valid(self) -> bool:
         return self.__check_side(self.source) and self.__check_side(self.destination)
+
+
+    def __check_side(self, side):
+        husbands = {person[0] for person in side if person.endswith('H')}
+        wives = {person[0] for person in side if person.endswith('W')}
+        for wife in wives:
+            # wife is without her husband and there are other husbands
+            if wife not in husbands and husbands:
+                return False
+        return True
