6188117becacbab8bec75ed92e0498b1424bacdc
Shehab Yasser <shehabyasser@gmail.com>
2024-07-25 17:28:41 -0400

    Updated river-crossing-puzzle.py

--- river-crossing-puzzle.py
+++ river-crossing-puzzle.py
@@ -2,3 +2,6 @@ class State:
     def __init__(self, source: set, destination: set) -> None:
         self.source = frozenset(source)
         self.destination = frozenset(destination)
+    
+    def is_terminal_state(self) -> bool:
+        return not self.source
