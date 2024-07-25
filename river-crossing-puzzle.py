6affd9e641189e939a52211936838f368b878f7a
Shehab Yasser <shehabyasser@gmail.com>
2024-07-25 17:34:20 -0400

    Updated river-crossing-puzzle.py

--- river-crossing-puzzle.py
+++ river-crossing-puzzle.py
@@ -8,26 +8,6 @@ class State:
 
     def is_valid(self) -> bool:
         return self.__check_side(self.source) and self.__check_side(self.destination)
-
-    def __check_side(self, side):
-        husbands = {person[0] for person in side if person.endswith('H')}
-        wives = {person[0] for person in side if person.endswith('W')}
-        for wife in wives:
-            # wife is without her husband and there are other husbands
-            if wife not in husbands and husbands:
-                return False
-        return True
-    
-    def __str__(self) -> str:
-        return '\nsource: ' + ' '.join(self.source) +  '\ndestination: ' + ' '.join(self.destination)
-    
-    def __eq__(self, other):
-        if not isinstance(other, State):
-            return NotImplemented
-        return self.source == other.source and self.destination == other.destination
-
-    def __hash__(self):
-        return hash((self.source, self.destination))
     
 
     def move_entity(self, source, destination, entities):
@@ -59,3 +39,23 @@ class State:
                         result.append(self.move_entity(source, destination, ['boat', first_person, second_person]))
 
         return result
+    
+    def __check_side(self, side):
+        husbands = {person[0] for person in side if person.endswith('H')}
+        wives = {person[0] for person in side if person.endswith('W')}
+        for wife in wives:
+            # wife is without her husband and there are other husbands
+            if wife not in husbands and husbands:
+                return False
+        return True
+    
+    def __str__(self) -> str:
+        return '\nsource: ' + ' '.join(self.source) +  '\ndestination: ' + ' '.join(self.destination)
+    
+    def __eq__(self, other):
+        if not isinstance(other, State):
+            return NotImplemented
+        return self.source == other.source and self.destination == other.destination
+
+    def __hash__(self):
+        return hash((self.source, self.destination))
