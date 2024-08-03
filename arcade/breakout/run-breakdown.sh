e0b048ce8db7b296a66825d79c1c4c25546d83c8
Shehab Yasser <shehabyasser@gmail.com>
2024-08-03 17:01:27 -0400

    Deleted breakdown.sh

--- arcade/breakout/run-breakdown.sh
+++
@@ -1,5 +0,0 @@
-curl -OL https://huggingface.co/cleanrl/BreakoutNoFrameskip-v4-dqn_atari-seed1/raw/main/pyproject.toml
-curl -OL https://huggingface.co/cleanrl/BreakoutNoFrameskip-v4-dqn_atari-seed1/raw/main/poetry.lock
-poetry install --all-extras
-
-curl -OL https://huggingface.co/cleanrl/BreakoutNoFrameskip-v4-dqn_atari-seed1/raw/main/dqn.py
