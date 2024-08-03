454390c0163eb91b520f37ac6cc9ad03ccc92a52
Shehab Yasser <shehabyasser@gmail.com>
2024-08-03 17:26:14 -0400

    Added baselines test

--- arcade/test_baselines.py
+++ arcade/test_baselines.py
@@ -8,24 +8,7 @@ env = gym.make('LunarLander-v2', render_mode='human')
 env = Monitor(env)
 env = DummyVecEnv([lambda: env])
 
-models_dir = "models/PPO"
-if not os.path.exists(models_dir):
-    os.makedirs(models_dir)
-
-logdir = "logs"
-if not os.path.exists(logdir):
-    os.makedirs(logdir)
-
-model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
-
-timesteps = 10000
-for i in range(10):
-    model.learn(total_timesteps=timesteps, reset_num_timesteps=False, tb_log_name="PPO")
-    model.save(f"{models_dir}/{timesteps * i}")
-
-model.save("final_ppo_lunar")
 model = PPO.load("final_ppo_lunar")
-
 obs = env.reset()
 
 for i in range(3000):
