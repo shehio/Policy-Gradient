ccdf55ef426f9240a6df97160d3021eb4c7bdb38
Shehab Yasser <shehabyasser@gmail.com>
2024-08-04 07:21:19 -0400

    Updated train/test files

--- arcade/baselines/test_baselines.py
+++ arcade/baselines/test_baselines.py
@@ -1,26 +1,7 @@
-import gymnasium as gym
 from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.common.monitor import Monitor
-
-def create_environment(name: str):
-    env = gym.make(name, render_mode='human')
-    env = Monitor(env)
-    env = DummyVecEnv([lambda: env])
-    return env
-
-def render_model(env, model, timesteps):
-    obs = env.reset()
-    for i in range(timesteps):
-        action, _states = model.predict(obs)
-        obs, rewards, dones, info = env.step(action)
-        if i % 100 == 0:
-            env.render()
-    env.close()
-
+from helpers import create_environment, render_model
 
 env_name = 'LunarLander-v2'
-env = create_environment(env_name)
+env = create_environment(name=env_name, render=True)
 model = PPO.load('final_ppo_lunar')
 render_model(env, model, 10 * 1000)
-
--- arcade/baselines/train_baselines.py
+++ arcade/baselines/train_baselines.py
@@ -1,26 +1,5 @@
-import gymnasium as gym
+from helpers import create_directory_if_not_exists, create_environment, train
 from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.common.monitor import Monitor
-import os
-
-def create_environment(name: str):
-    env = gym.make(name)
-    env = Monitor(env)
-    env = DummyVecEnv([lambda: env])
-    return env
-
-def create_directory_if_not_exists(directory: str):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-def train(model, timesteps, epochs, tensorboard_log_name, model_directory):
-    for i in range(epochs):
-        model.learn(total_timesteps=timesteps, reset_num_timesteps=False, tb_log_name=tensorboard_log_name)
-        model.save(f"{model_directory}/{timesteps * i}")
-
-    return model
-
 
 env_name = 'LunarLander-v2'
 log_directory = 'logs'
