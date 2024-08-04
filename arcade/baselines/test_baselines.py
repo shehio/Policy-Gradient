c9702fbd2679725ec83b86210323c3842f2a79a8
Shehab Yasser <shehabyasser@gmail.com>
2024-08-04 07:07:26 -0400

    Refactored test_baselines.py

--- arcade/baselines/test_baselines.py
+++ arcade/baselines/test_baselines.py
@@ -3,16 +3,24 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.monitor import Monitor
 
-env = gym.make('LunarLander-v2', render_mode='human')
-env = Monitor(env)
-env = DummyVecEnv([lambda: env])
+def create_environment(name: str):
+    env = gym.make(name, render_mode='human')
+    env = Monitor(env)
+    env = DummyVecEnv([lambda: env])
+    return env
 
-model = PPO.load("final_ppo_lunar")
-obs = env.reset()
+def render_model(env, model, timesteps):
+    obs = env.reset()
+    for i in range(timesteps):
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = env.step(action)
+        if i % 100 == 0:
+            env.render()
+    env.close()
 
-for i in range(3000):
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    if i % 30 == 0:
-        env.render()
-env.close()
+
+env_name = 'LunarLander-v2'
+env = create_environment(env_name)
+model = PPO.load('final_ppo_lunar')
+render_model(env, model, 10 * 1000)
+
