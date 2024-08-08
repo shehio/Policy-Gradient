6ec18c4d852dab962dada986fbb2e812da9b9bd1
Shehab Yasser <shehabyasser@gmail.com>
2024-08-08 14:09:23 -0400

    Updated helpers.py

--- arcade/baselines/helpers.py
+++ arcade/baselines/helpers.py
@@ -6,6 +6,7 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.atari_wrappers import AtariWrapper
 import os
+import time
 
 def create_atari_environment(name: str, render: bool, render_fps = 60):
     render_mode = 'human' if render else None
@@ -27,10 +28,15 @@ def create_environment(name: str, render: bool, render_fps = 60):
     return env
 
 def train(model, timesteps, epochs, tensorboard_log_name, model_directory):
+    start_time = time.time()
     for i in range(epochs):
         model.learn(total_timesteps=timesteps, reset_num_timesteps=False, tb_log_name=tensorboard_log_name)
         model.save(f"{model_directory}/{timesteps * i}")
 
+    end_time = time.time()
+
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time / 60} minutes")
     return model
 
 def render_model(env, model, timestep: int, frame_skip: int = 10):
