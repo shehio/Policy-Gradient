9173b1aba1aa4036d3ebb4ebc7a40a16f0d9dc4d
shehio <shehabyasser@gmail.com>
2025-07-12 12:59:48 +0300

    Added DQN config models

---
+++ src/dqn/config/learning_config.py
@@ -0,0 +1,4 @@
+class LearningConfig:
+    def __init__(self, gamma: float = 0.97, alpha: float = 0.00025):
+        self.gamma = gamma
+        self.alpha = alpha 
---
+++ src/dqn/config/model_config.py
@@ -0,0 +1,13 @@
+class ModelConfig:
+    def __init__(self, save_models: bool = True,
+                 model_path: str = "./models/pong-cnn-",
+                 save_model_interval: int = 10,
+                 train_model: bool = True,
+                 load_model_from_file: bool = True,
+                 load_file_episode: int = 0):
+        self.save_models = save_models
+        self.model_path = model_path
+        self.save_model_interval = save_model_interval
+        self.train_model = train_model
+        self.load_model_from_file = load_model_from_file
+        self.load_file_episode = load_file_episode 
