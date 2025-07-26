9119006ddd5b2351b0112fb1cde1f105b6971bf7
shehio <shehabyasser@gmail.com>
2025-07-26 07:41:53 +0300

    Added Pong files

--- atari/baselines/pong_test.py
+++ atari/baselines/pong_test.py
@@ -3,5 +3,5 @@ from helpers import create_atari_environment, render_model
 
 env_name = 'Pong-v0'
 env = create_atari_environment(name=env_name, render=True)
-model = DQN.load('pong_ppo.zip')
+model = DQN.load('models/PPO/Pong_step_2400000')
 render_model(env, model, 10 * 1000)
--- atari/baselines/pong_train.py
+++ atari/baselines/pong_train.py
@@ -1,7 +1,7 @@
-from helpers import create_directory_if_not_exists, create_atari_environment, train
+from helpers import create_directory_if_not_exists, create_atari_environment, train, SaveEveryStepCallback
 from stable_baselines3 import DQN
 
-env_name = 'Pong-v0'
+env_name = 'ALE/Pong-v5'
 log_directory = 'logs'
 model_directory = 'models/PPO'
 
@@ -12,8 +12,10 @@ create_directory_if_not_exists(log_directory)
 # Use 'CnnPolicy' for image-based observations like Atari Breakout
 model = DQN('CnnPolicy', env, verbose=1, tensorboard_log=log_directory)
 
-timesteps = 10000
+save_callback = SaveEveryStepCallback(save_freq=100_000, save_path=model_directory, game_name='Pong')
+
+timesteps = 100_000_000
 epochs = 1000
-model = train(model, timesteps, epochs, env_name, model_directory)
-model.save('pong_ppo')
+model = train(model, timesteps, epochs, env_name, model_directory, callback=save_callback)
+model.save(f'final_ppo_breakout_epochs_{epochs}_timesteps_{timesteps}')
 
