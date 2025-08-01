80038a028c793fae08107668df8b4a0188304e45
shehio <shehabyasser@gmail.com>
2025-08-01 09:55:49 +0300

    Removed some baseline env specific train/test

--- atari/baselines/pacman_test.py
+++
@@ -1,7 +0,0 @@
-from stable_baselines3 import PPO
-from helpers import create_atari_environment, render_model
-
-env_name = 'ALE/MsPacman-v5'
-env = create_atari_environment(name=env_name, render=True)
-model = PPO.load('models/PPO/MsPacman_step_2900000')
-render_model(env, model, 10 * 1000)
