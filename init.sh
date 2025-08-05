4e245ebaa08bc04fd4955f1df4ee69c7ee5fdd2c
shehio <shehabyasser@gmail.com>
2025-08-05 15:10:39 +0300

    Added pre-commit

--- init.sh
+++ init.sh
@@ -3,9 +3,10 @@ brew install swig
 brew install ffmpeg
 
 # Create virtual environment
-python3.11 -m venv venv
+python3.13 -m venv venv
 source ./venv/bin/activate
 which python
 which pip
 pip install -r requirements.txt
 AutoROM --accept-license
+pre-commit install
